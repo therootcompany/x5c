662e1888ecc6cedfc52c543ca72b67e7fe2047db
AJ ONeal <aj@therootcompany.com>
2024-09-07 23:50:46 -0600

    fix: add ./static/fileviewer.js to go embed

--- static/embed.go
+++ static/embed.go
@@ -4,6 +4,6 @@ import "embed"
 
 // curl -L -O https://andybrewer.github.io/mvp/mvp.css
 
-//go:embed index.html mvp.css
+//go:embed index.html mvp.css fileviewer.js
 var FS embed.FS
 var Prefix = ""
